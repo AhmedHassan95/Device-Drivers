a41cb63f938a07c97beb9c05fb5a2551a34daf0b
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-05-18 14:09:12 +0200

    Update led.h

--- Device Drivers/External Device Drivers/1.LED/led.h
+++ Device Drivers/External Device Drivers/1.LED/led.h
@@ -52,13 +52,13 @@ void LED_init(void);
  *
  * [Description]: Function to turn on the LED
  *
- * [Args]:	  None
+ * [Args]:	   None
  *
- * [in]		None
+ * [in]		 None
  *
- * [out]	None
+ * [out]	 None
  *
- * [in/out]	 None
+ * [in/out]	  None
  *
  * [Returns]:     None
  *******************************************************************************/
@@ -67,15 +67,15 @@ void LED_turnOn(void);
 /*******************************************************************************
  * [Function Name]: LED_turnOff
  *
- * [Description]: 	Function to turn off the LED
+ * [Description]: Function to turn off the LED
  *
- * [Args]:			None
+ * [Args]:	None
  *
- * [in]			  	None
+ * [in]			 None
  *
- * [out]		  	None
+ * [out]		 None
  *
- * [in/out]		 	None
+ * [in/out]		None
  *
  * [Returns]:       None
  *******************************************************************************/
