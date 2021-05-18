54a8213a4d4ff9b303ec1795293ed8497af33400
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-05-18 14:43:25 +0200

    Update buzzer.h

--- Device Drivers/External Device Drivers/3.BUZZER/buzzer.h
+++ Device Drivers/External Device Drivers/3.BUZZER/buzzer.h
@@ -37,9 +37,9 @@
  *
  * [Args]:	  None
  *
- * [in]		 None
+ * [in]		  None
  *
- * [out]	 None
+ * [out]	  None
  *
  * [in/out]	  None
  *
@@ -54,11 +54,11 @@ void BUZZER_init(void);
  *
  * [Args]:	  None
  *
- * [in]		 None
+ * [in]		  None
  *
- * [out]	None
+ * [out]	  None
  *
- * [in/out]	 None
+ * [in/out]	  None
  *
  * [Returns]:     None
  *******************************************************************************/
